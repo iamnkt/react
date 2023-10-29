28d1e79509a6d076428624d5c34ec96a05b181c8
iamnkt <sg23mj@gmail.com>
2023-10-30 02:59:48 +0300

    feat: add no cards message render

--- pokemon-app/src/components/error-boundary/errorBoundary.tsx
+++ pokemon-app/src/components/error-boundary/errorBoundary.tsx
@@ -1,5 +1,6 @@
 import { Component, ErrorInfo } from 'react';
 import { ErrorProps, ErrorState } from '../../types/types';
+import './errorBoundary.css';
 
 class ErrorBoundary extends Component<ErrorProps, ErrorState> {
   public state: ErrorState = { hasError: false };
@@ -15,7 +16,7 @@ class ErrorBoundary extends Component<ErrorProps, ErrorState> {
 
   public render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong</h1>;
+      return <h1 className="error-caption">Something went wrong</h1>;
     }
 
     return this.props.children;
--- pokemon-app/src/components/view/view.tsx
+++ pokemon-app/src/components/view/view.tsx
@@ -7,17 +7,27 @@ class View extends Component<ViewProps, { items: Data[] }> {
     super(props);
   }
 
-  public render(): JSX.Element {
+  public renderElement(): JSX.Element {
+    if (this.props.data.length)
+      return (
+        <div className="items">
+          {this.props.data.map((item: Data) => (
+            <div className="item" key={item.id}>
+              <h4 className="title">{item.name}</h4>
+              <img className="image" src={item.image}></img>
+            </div>
+          ))}
+        </div>
+      );
     return (
-      <div className="items">
-        {this.props.data.map((item: Data) => (
-          <div className="item" key={item.id}>
-            <h4 className="title">{item.name}</h4>
-            <img className="image" src={item.image}></img>
-          </div>
-        ))}
+      <div className="nothing">
+        <h3 className="nothing-caption">No cards were found</h3>
       </div>
     );
+  }
+
+  public render(): JSX.Element {
+    return this.renderElement();
   }
 }
 
