ee687683bc8472d7827a8d8bd22c9eb488cce673
iamnkt <sg23mj@gmail.com>
2023-11-26 19:38:55 +0300

    feat: add query params on page first loading

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -1,5 +1,6 @@
 import { CardsData, DetailedCard } from '@/types/types';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Cards from '../cards/cards';
 import Details from '../details/details';
 import Pages from '../pages/pages';
@@ -14,6 +15,17 @@ interface MainComp {
 
 const Main: React.FC<MainComp> = ({ cardsData, detailedCardData }) => {
   const router = useRouter();
+
+  useEffect(() => {
+    if (!router.query.name && !router.query.page && !router.query.limit) {
+      router.query.name = '';
+      router.query.page = '1';
+      router.query.limit = '8';
+      router.push(router);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const { query, pathname } = router;
 
   const handleMainContainerClick = () => {
