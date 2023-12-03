479e0c9a78bcc4d1e81ace317fad785ee8d91cff
iamnkt <106612825+iamnkt@users.noreply.github.com>
2023-12-03 03:05:29 +0300

    Delete forms directory

--- forms/src/components/ReactHookForm/ReactHookForm.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const ReactHookForm: React.FC = () => {
-  return <div>form2</div>;
-};
--- forms/src/store/reducers/countriesSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-export const countriesSlice = createSlice({
-  name: 'countries',
-  initialState: ['UK', 'France', 'Spain', 'Germany'],
-  reducers: {},
-});
-
-export default countriesSlice.reducer;
